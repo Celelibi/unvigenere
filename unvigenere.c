49cecc2e26d36141efc10718f9fa5e12f322ae8d
Celelibi <celelibi@gmail.com>
2014-03-06 05:43:24 +0100

    unvigenere: Just cipher / decipher
    Cracking is not yet implemented, but still this program makes something
    useful. :)
    
    Signed-off-by: Celelibi <celelibi@gmail.com>

--- unvigenere.c
+++ unvigenere.c
@@ -1,6 +1,216 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#include "getopthelp.h"
+#include "filtered_string.h"
+#include "vigenere.h"
+#include "misc.h"
 
 
-int main(void) {
+
+enum action {
+	ACTION_ENCRYPT,
+	ACTION_DECRYPT,
+	ACTION_CRACK
+};
+
+
+static const struct goh_option opt_desc[] = {
+	{"input", 'i', GOH_ARG_REQUIRED, 'i',
+		"Input file. May be - for stdin. Default to stdin."},
+	{"output", 'o', GOH_ARG_REQUIRED, 'o',
+		"Output file. May be - for stdout. Default to stdout."},
+	{"encrypt", 'e', GOH_ARG_REFUSED, 'e',
+		"Encrypt the data with the given key."},
+	{"decrypt", 'd', GOH_ARG_REFUSED, 'd',
+		"Decrypt the data with the given key."},
+	{"key", 'k', GOH_ARG_REQUIRED, 'k',
+		"Key used for encryption / decryption."}
+};
+
+
+
+
+static void crack(struct fs_ctx *s) {
+	(void)s;
+	printf("Cracking is not yet implemented.\n");
+}
+
+
+
+static void simple_action(struct fs_ctx *s, const char *key, enum action act) {
+	struct vigenere vig;
+
+	vig_init(&vig, s);
+	vig_add_charset(&vig, VIG_CHARSET_UPPER);
+	vig_add_charset(&vig, VIG_CHARSET_LOWER);
+
+	if (act == ACTION_ENCRYPT)
+		vig_encrypt(&vig, key);
+	else if (act == ACTION_DECRYPT)
+		vig_decrypt(&vig, key);
+	else
+		custom_error("Dafuq? simple_action called with an unknown action");
+
+	vig_fini(&vig);
+}
+
+
+
+/*
+ * Load a file into a buffer allocated with malloc. Must be freed by caller.
+ * The filename may be "-" to read from stdin.
+ */
+static char *read_file(const char *filename) {
+	FILE *fp = stdin;
+	char *buffer;
+	size_t buffer_size = 0;
+	size_t buffer_mem = 8;
+	size_t nr;
+	int err;
+
+	if (strcmp(filename, "-") != 0) {
+		fp = fopen(filename, "r");
+		if (fp == NULL)
+			system_error(filename);
+	}
+
+	buffer = malloc(buffer_mem * sizeof(*buffer));
+	if (buffer == NULL)
+		system_error("malloc");
+
+	do {
+		/* Grow the buffer if needed. */
+		if (buffer_size >= buffer_mem) {
+			buffer_mem *= 2;
+			buffer = realloc(buffer, buffer_mem * sizeof(*buffer));
+			if (buffer == NULL)
+				system_error("realloc");
+		}
+
+		nr = fread(buffer + buffer_size, 1, buffer_mem - buffer_size, fp);
+		buffer_size += nr;
+	} while (nr > 0);
+
+	if (ferror(fp))
+		system_error("fread");
+
+	if (strcmp(filename, "-") != 0) {
+		err = fclose(fp);
+		if (err == -1)
+			system_error("fclose");
+	}
+
+	return buffer;
+}
+
+
+
+/*
+ * Write some data to a file. May be "-" to write to stdout.
+ */
+static void write_file(const char *filename, const char *data) {
+	FILE *fp = stdout;
+	size_t len;
+	size_t nw;
+	int err;
+
+
+	if (strcmp(filename, "-") != 0) {
+		fp = fopen(filename, "w");
+		if (fp == NULL)
+			system_error(filename);
+	}
+
+	len = strlen(data);
+
+	nw = fwrite(data, 1, len, fp);
+	if (ferror(fp))
+		system_error("fwrite");
+
+	if (nw != len)
+		custom_error("Dafuq? Didn't write everything we should.");
+
+	if (strcmp(filename, "-") != 0) {
+		err = fclose(fp);
+		if (err == -1)
+			system_error("fclose");
+	}
+}
+
+
+
+int main(int argc, char **argv) {
+	struct goh_state st;
+	int opt;
+	struct fs_ctx s;
+	enum action action = ACTION_CRACK;
+	char *key = NULL;
+	const char *filenamein = "-";
+	const char *filenameout = "-";
+	char *text = NULL;
+
+
+	/* Parse the options. */
+	goh_init(&st, opt_desc, ARRAY_LENGTH(opt_desc), argc, argv, 1);
+	st.usagehelp = "[options]\n";
+
+	while ((opt = goh_nextoption(&st)) >= 0) {
+		switch (opt) {
+		case 'i':
+			filenamein = st.argval;
+			break;
+
+		case 'o':
+			filenameout = st.argval;
+			break;
+
+		case 'e':
+			action = ACTION_ENCRYPT;
+			break;
+
+		case 'd':
+			action = ACTION_DECRYPT;
+			break;
+
+		case 'k':
+			key = st.argval;
+			break;
+
+		default:
+			custom_error("Unrecognized option (shouldn't happen)");
+			break;
+		}
+	}
+
+
+	/* Common command line mistakes. */
+	if (st.argidx != argc)
+		custom_error("Useless argument %s", argv[st.argidx]);
+
+	goh_fini(&st);
+
+	if (action != ACTION_CRACK && key == NULL)
+		custom_error("Encryption and decryption take a --key.");
+
+	if (action == ACTION_CRACK && key != NULL)
+		custom_error("--key need either --encrypt or --decrypt");
+
+	/* Start to do the job. */
+	text = read_file(filenamein);
+	fs_init(&s, text, FS_CHARSET_ALPHA);
+
+	if (action == ACTION_CRACK)
+		crack(&s);
+	else
+		simple_action(&s, key, action);
+
+	write_file(filenameout, text);
+
+	fs_fini(&s);
+	free(text);
+
+
 	return EXIT_SUCCESS;
 }
