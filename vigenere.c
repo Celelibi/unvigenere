05f267cf254581884198d5518a1a2c800263295f
Celelibi <celelibi@gmail.com>
2014-04-18 05:26:22 +0200

    vigenere: Factor encrypt and decrypt functions
    The code of those two functions was a dumb copy/paste with a sign
    change. Now the code is in a single function with an additional
    parameter and the encrypt and decrypt functions are just wrappers.
    
    Signed-off-by: Celelibi <celelibi@gmail.com>

--- vigenere.c
+++ vigenere.c
@@ -30,8 +30,7 @@ static char shift_char(struct fs_ctx *str, char c, int s) {
 
 
 
-/* TODO: refactor vig_encrypt and vig_decrypt */
-void vig_encrypt(struct fs_ctx *str, const char *key) {
+static void vigenere_compute(struct fs_ctx *str, const char *key, int sign) {
 	struct fs_ctx fskey;
 	size_t klen, i;
 	char *ntext;
@@ -48,6 +47,10 @@ void vig_encrypt(struct fs_ctx *str, const char *key) {
 	i = 0;
 	while (ntext[i] != '\0') {
 		int shift = cs_ord(str->charset, key[i % klen]);
+
+		if (sign < 0)
+			shift = -shift;
+
 		ntext[i] = shift_char(str, ntext[i], shift);
 		i++;
 	}
@@ -59,28 +62,12 @@ void vig_encrypt(struct fs_ctx *str, const char *key) {
 
 
 
+void vig_encrypt(struct fs_ctx *str, const char *key) {
+	vigenere_compute(str, key, 1);
+}
+
+
+
 void vig_decrypt(struct fs_ctx *str, const char *key) {
-	struct fs_ctx fskey;
-	size_t klen, i;
-	char *ntext;
-
-	/* I know filtered_string won't modify key. */
-	fs_init(&fskey, (char *)key, str->charset);
-	key = fskey.norm;
-	klen = strlen(key);
-
-	/* Duplicate str->norm so that the struct str isn't transiently
-	 * inconsistent. */
-	ntext = strdup(str->norm);
-
-	i = 0;
-	while (ntext[i] != '\0') {
-		int shift = cs_ord(str->charset, key[i % klen]);
-		ntext[i] = shift_char(str, ntext[i], -shift);
-		i++;
-	}
-
-	fs_replace(str, ntext);
-	free(ntext);
-	fs_fini(&fskey);
+	vigenere_compute(str, key, -1);
 }
